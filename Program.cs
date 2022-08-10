4e9c450ca1b9b0d94d820995a2f6f8e4d981f4ca
Michalis Christodoulou <mchristodoulou@vivawallet.com>
2022-08-10 17:50:58 +0300

    program and provider

--- Program.cs
+++ Program.cs
@@ -10,4 +10,8 @@ using TradingEngineServer.Core;
 // This will build my entire dependency injection container and hosted service for me in one line (very clean and easy)
 // I use "using" here because I want to dispose of this engine when the program terminates
 // Used to get rid of unmanaged resources like database connections, streams, etc.
+
 using var engine = TradingEngineServerHostBuilder.BuildTradingEngineServer();
+TradingEngineServerServiceProvider.ServiceProvider = engine.Services;
+
+// This engine is now an IHost (no longer of type TradingEngineServer)
---
+++ TradingEngineServerServiceProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingEngineServer.Core
+{
+    public static class TradingEngineServerServiceProvider
+    {
+        // We are going to have this IServiceProvider accessible to us throughout the entire application in case we need it because its class is static 
+        //and because this member variable is static
+        public static IServiceProvider ServiceProvider { get; set; }
+            
+    }
+}
