401ceb82695a87d0fa1b509d502f24d813b95c02
Michalis Christodoulou <mchristodoulou@vivawallet.com>
2022-08-10 17:43:24 +0300

    program.cs

--- Program.cs
+++ Program.cs
@@ -7,3 +7,7 @@ using Microsoft.Extensions.Hosting;
 
 using TradingEngineServer.Core;
 
+// This will build my entire dependency injection container and hosted service for me in one line (very clean and easy)
+// I use "using" here because I want to dispose of this engine when the program terminates
+// Used to get rid of unmanaged resources like database connections, streams, etc.
+using var engine = TradingEngineServerHostBuilder.BuildTradingEngineServer();
