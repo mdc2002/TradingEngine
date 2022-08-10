2219d1d17e8f56b50e4735b877e7e6c1f7625c81
Michalis Christodoulou <mchristodoulou@vivawallet.com>
2022-08-10 16:10:59 +0300

    19:18

---
+++ TradingEngineServer.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TradingEngineServer.Core
+{
+    class TradingEngineServer : BackgroundService, ITradingEngineServer
+    {
+        public TradingEngineServer(ILogger)
+        {
+
+
+        }
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+
+
+            }
+        }
+    }
+}
